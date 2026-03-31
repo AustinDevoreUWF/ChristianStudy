aa42bdd1452c395cdc7bbbcb483fc39fd8c66429
AustinDevoreUWF <ard78@students.uwf.edu>
2026-03-31 17:10:54 -0500

    Added methods to the discussion and comment repoistory layer

--- domain/posts.ts
+++ domain/posts.ts
@@ -4,11 +4,12 @@ export class Discussion {
     //Variables
     id: number;
     title: string;
-    text: string
+    text: string;
+    authorId: number;
     //authorId: number; add this
 
     //Constructor1.Default/
-    constructor(discussionId:number, discussionTitle:string, discussionText:string){
+    constructor(discussionId:number, discussionTitle:string, discussionText:string, authorId:number){
         if(!discussionId)throw new Error("A discussionId was not provided, Im low Level!");
         if(!discussionTitle)throw new Error("A discussionTitle was not provided!");
         if(!discussionText) throw new Error("Discussion Text was not provided!")
@@ -17,19 +18,20 @@ export class Discussion {
         this.id = discussionId;
         this.title = discussionTitle;
         this.text = discussionText;
+        this.authorId = authorId
     }
     
     //Entity as String
     toString(): string {
-        return `Discussion(discussionID)=${this.id}, discussionTitle=${this.title}, discussionText=${this.text}`
+        return `Discussion(discussionID)=${this.id}, discussionTitle=${this.title}, discussionText=${this.text}, authorId=${this.authorId}`
     };
 }
 export class Comment{
     id: number;
     title: string;
     text: string;
-
-    constructor(commentId: number, commentTitle: string, commentText: string,){
+    authorId: number;
+    constructor(commentId: number, commentTitle: string, commentText: string, authorId:number){
         //checks
         if(!commentId)throw new Error("A commentId was not provided, Im low Level!");
         if(!commentTitle)throw new Error("A commentTitle was now provided!");
@@ -37,9 +39,10 @@ export class Comment{
         //mapping
         this.id = commentId
         this.title = commentTitle
-        this.text = commentText 
+        this.text = commentText
+        this.authorId = authorId
     }
     toString():string{
-        return `Comment(commentID)=${this.id}, commentTitle=${this.title}, commentText=${this.text}`
+        return `Comment(commentID)=${this.id}, commentTitle=${this.title}, commentText=${this.text}, authorId=${this.authorId}`
     }
 }
--- repository/posts.ts
+++ repository/posts.ts
@@ -5,26 +5,59 @@ import { Comment } from "@/domain/posts";
 export interface DiscussionRepo{
     //Find the discussion By Id
     findById(id: number): Promise<Discussion | null>
-    findByTitle(title: number):Promise<Discussion | null>
-    save();
+    findByTitle(title: string):Promise<Discussion[] | null>
+    findByauthorId(authorId:number):Promise<Discussion[] | null>
 }
-export interface CommentRepo{
-
-}
-
 
 export class PrismaDiscussionRepo implements DiscussionRepo {
-    
+    //Find by the unique ID(only returns 1)
     async findById(id:number): Promise<Discussion | null>{
         const data = await prisma.discussion.findUnique({
             where: {id: id},
         });
         if(!data)return null;
-        return new Discussion(data.id, data.title!, data.text!)
+        return new Discussion(data.id, data.title!, data.text!, data.authorId!)
     }
-    findByTitle(title: number):Promise<Discussion | null>
-    save();
+    //Find by Title(Can return many)
+    async findByTitle(title: string):Promise<Discussion[] | null>{
+        const data = await prisma.discussion.findMany({
+            where: { title },
+        });
+        return data.map(d => new Discussion(d?.id,d.title!,d.text!,d.authorId!)
+        )
+    }
+    //returns an array of all Discussions made by a User
+    async findByauthorId(authorId: number): Promise<Discussion[] | null> {
+        const data = await prisma.discussion.findMany({
+            where: {authorId},
+        });
+        return data.map(d => 
+            new Discussion(d.id, d.title!, d.text!, d.authorId!)
+  );
+    }
 }
-export class PrismaCommentRepo{
 
+
+
+export interface CommentRepo{
+    findById(id:number):Promise<Comment | null>
+    findByauthorId(authorId : number):Promise<Comment | null>
+    findAllCommentsUnderDiscussion(discussionId:number):Promise<Comment[]| null>
+}
+export class PrismaCommentRepo implements CommentRepo{
+    async findById(id:number):Promise<Comment|null>{
+        const data = await prisma.comment.findUnique({
+            where: {id: id},
+        });if(!data)return null;
+        return new Comment(data.id!, data.text!, data.title!, data.authorId!)
+    }
+    async findByauthorId(authorId:number):Promise<Comment|null>{
+        const data = await prisma.comment.findMany({
+            where: {authorId},
+        });
+        return
+    }
+    findAllCommentsUnderDiscussion(discussionId:number):Promise<Comment[]|null>{
+        return
+    }
 }

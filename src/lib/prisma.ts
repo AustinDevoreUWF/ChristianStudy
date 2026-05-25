e50dfb1c9b164d364ae63b556bb7033896134bb4
AustinDevoreUWF <ard78@students.uwf.edu>
2026-05-25 15:38:02 -0500

    Unending Issue

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,7 @@
 import { PrismaClient } from "../generated/prisma";
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { neon, neonConfig } from "@neondatabase/serverless";
-
-neonConfig.fetchConnectionCache = true;
-
-const sql = neon(process.env.DATABASE_URL!);
-const adapter = new PrismaNeon(sql as any);
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }

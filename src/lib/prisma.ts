3e948010b4c54c211dac5135b78f9547f1d79085
AustinDevoreUWF <ard78@students.uwf.edu>
2026-05-25 15:44:39 -0500

    Still going

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from "../generated/prisma";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "@neondatabase/serverless";
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
+const adapter = new PrismaNeon(pool as any);
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
